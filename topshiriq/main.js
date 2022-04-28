3d00fe678fecb525c626751bd102b1296706830b
Ayyubxon007 <editedit879@gmail.com>
2022-04-28 16:27:18 +0500

    first commit

---
+++ topshiriq/main.js
@@ -0,0 +1,44 @@
+// document.getElementById("no").innerHTML = "Yoq joydan yozuv";
+// function color(){
+//     document.getElementById("demo").style.color = "red";
+// }
+// function fs(){
+//     document.getElementById("demo").style.fontSize = "50px";
+// }
+// function fs1(){
+//     document.getElementById("demo").style.fontSize = "16px";
+// }
+// function none(){
+//     document.getElementById("demo").style.display = "none";
+// }
+// function block(){
+//     document.getElementById("demo").style.display = "block";
+// }
+// let name= "Ayyubxon"
+// let name1="Shuhratbekov"
+// let age= "15" 
+// function main(){
+//     document.getElementById("main").innerHTML = "Ayyubxon" +"<br>"+"SHuhratbekov "+"<br>"+"My age 15";
+// }
+// let z=10023328;
+// let x=345763453;
+// let y = z*x;
+// console.log(y)
+// let z= 105;
+// let w= 105;
+// console.log(z>=w);
+
+var age = prompt("Balingizni ayting")
+if (age<70){
+    document.getElementById("demo").innerHTML =  "Imtihondan ota olmadingiz hafa bolmang!"
+} else if(age>=120){
+    document.getElementById("demo").innerHTML =  "Imtihondan otiingiz!"
+}
+ else if (age<80){
+    document.getElementById("demo").innerHTML =  "Superkontragka kirdinggiz"
+}
+else
+{
+    document.getElementById("demo").innerHTML =  "Kontragka kirdinggiz!"
+}
+    
---
+++ topshiroq1/main.js
@@ -0,0 +1,79 @@
+//  let age = prompt("Sizda necha pul bor?")
+
+//  if(age>=86){
+//      document.getElementById("demo").innerHTML = ("Siz imtihondan o'tingiz 5")
+//  }
+//   else if (age>=75){
+//      document.getElementById("demo").innerHTML = ("Siz imtihondan o'tingiz 4")
+//  }
+//   else if(age<=30)
+//  {
+//       document.getElementById("demo").innerHTML = "Sizning Bahoingiz 3"
+//   } 
+//   else if(age>=30)
+//   {
+//        document.getElementById("demo").innerHTML = "Sizning Bahoingiz 3"
+//    } 
+//   else 
+//   {
+//      document.getElementById("demo").innerHTML = ("Siz imtihondan o'tingiz")
+//   }
+    
+//  switch(age){
+//      case "10":case"20":case "25":
+//         document.getElementById("demo").innerHTML = ("Siz bu pulingizga |5ta non|,0.5kilo go'sht|,2 kilo olma ola olasan!")
+//         break;
+//         case"50":
+//         document.getElementById("demo").innerHTML = ("Siz tort sotib olishingiz mumkun</>")
+//  }
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
+// console.log(11);
+// console.log(12);
+// console.log(13);
+// console.log(14);
+// console.log(15);
+// console.log(16);
+// console.log(17);
+// console.log(18);
+// console.log(19);
+// console.log(20);
+// console.log(21);
+// console.log(22);
+// console.log(23);
+// console.log(24);
+// console.log(25);
+// console.log(26);
+// console.log(27);
+// console.log(28);
+// console.log(29);
+// console.log(30);
+var age = 1000;
+var x = 0;
+// while(x<age){
+//     x++
+//     console.log(x)
+// }
+// <>Bu consulda ketma ket  </>
+// bu for dagi usuli
+// for(var i=0;i<age;i++){
+//     console.log(i)
+// // }
+
+// for (var j=100;j>=age;j=j-5){
+//     console.log(j)
+// }
+
+
+
+
+
+
